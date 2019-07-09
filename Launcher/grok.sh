57be7f601a8aa6b97eec8f25130f6046cba77b91
iculus <mgsoroka@gmail.com>
2019-07-09 09:23:56 +0800

    grok test

--- Launcher/grok.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#CLEO 01
-/home/admin/ngrok tcp 22 -remote-addr=1.tcp.ngrok.io:24423
-
-exit 0
--- Launcher/grokweb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#CLEO 01
-/home/admin/ngrok http -subdomain logintowificleo01 80 
-
-exit 0
